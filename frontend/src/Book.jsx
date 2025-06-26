4067f6314bc5726a404d64d3d6231d13559e4d44
marksivan <marktamakloe2020@gmail.com>
2025-06-26 14:34:49 -0700

    added page components

---
+++ frontend/src/Book.jsx
@@ -0,0 +1,23 @@
+import './Book.css'
+
+
+function Book(props) {
+    return (
+        <div className='book'>
+            <div className='cover-image'>
+
+            </div>
+
+            <div className='title'>
+            </div>
+
+            <div className='author'>
+
+            </div>
+
+        </div>
+
+    )
+}
+
+export default Book;
