500edcf4dd5a6251a392c412a7ce137d06d44378
marksivan <marktamakloe2020@gmail.com>
2025-06-27 10:22:57 -0700

    set up App header

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+
+function App() {
+  return (
+    <div className="App">
+        <div className='app-header'>
+            <h1>Story Stack</h1>
+        </div>
+
+    </div>
+  )
+}
+
+export default App;
