220235d1506d027bd687810b37a2ccf5f7330ed3
marksivan <marktamakloe2020@gmail.com>
2025-06-29 17:40:28 -0700

    added search functionality

---
+++ frontend/src/Search.jsx
@@ -0,0 +1,49 @@
+import './Search.css'
+
+import { useState } from 'react'
+
+function Search( {onSearch}){
+    const [query, setQuery] = useState(''); //query stores what's typed in the search bar
+    const [loading, setLoading] = useState(false);   // loading is used to show "Searching..." on the button while the API call runs
+
+
+    const handleSubmit =  async (e) => {
+        e.preventDefault(); // Prevent default form submission behavior
+        if (!query.trim()) return; // avoid empty searches
+
+        setLoading(true); // Set loading to true to show "Searching..." on the button
+
+        try {
+            // Fetch books from the Google Books API
+            const response = await fetch(
+                `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(query)}`
+            );
+            const data = await response.json();
+
+            const books = data.items || []; // Get the books from the response  or empty array if no books found
+
+            onResults(books);
+        } catch (err){
+            console.error('Error fetching books:', err);
+            onResults([]); // If there's an error, show an empty array of books
+        } finally {
+            setLoading(false); // Set loading to false to hide "Searching..." on the button
+        }
+    }
+
+    return(
+     <form onSubmit={handleSubmit}>
+        <input type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder='Search books....'
+         />
+         <button type='submit'>
+            Search
+         </button>
+     </form>
+    )
+
+}
+
+export default Search;
